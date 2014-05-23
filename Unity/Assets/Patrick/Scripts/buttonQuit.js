3f956045e7f7da91dd65920c995a1396c17b4422
Fuby <patrick.steidinger@hotmail.de>
2014-05-24 01:36:56 +0200

    Menue_v1
    3 von 7 Screens:
    -Titelbildschirm - done
    -Movie 1 - done
    -Start - done
    -Cabin
    -Map new
    -Movie 2
    -Credits

---
+++ Unity/Assets/Patrick/Scripts/buttonQuit.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var quitNormal : Texture2D;
+var quitHover  : Texture2D;
+
+function OnMouseEnter()
+{
+	guiTexture.texture = quitHover;
+}
+
+function OnMouseExit()
+{
+	guiTexture.texture = quitNormal;
+}
+
+function OnMouseDown()
+{
+	guiTexture.texture = quitHover;
+	Application.Quit();
+}
