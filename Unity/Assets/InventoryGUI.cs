312427ec2c59883d08df27288d0bd51a715bc1f8
2of3 <lindaschey@t-online.de>
2014-05-24 00:15:49 +0200

    Inventar mit Grundfunktionen

---
+++ Unity/Assets/InventoryGUI.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InventoryGUI : MonoBehaviour {
+
+
+	private bool showInventory = true;
+	private Rect InventoryWindowRect = new Rect((Screen.width *0.5f)-(Screen.width*0.4f)/2, Screen.height*0.9f , Screen.width*0.4f, Screen.height*0.1f);
+
+	static public  int inventorySize = 7;
+	//private Dictionary<int, string> inventoryNameDictionary;
+	//public static Dictionary<int, Texture2D> inventoryNameDictionary = new Dictionary<int, Texture2D>()
+	public List<ItemCreatorClass> inventoryList = new List<ItemCreatorClass>()
+	{
+		{null},
+		{null},
+		{null},
+		{null},
+		{null},
+		{null},
+		{null}
+	};
+
+	//ItemClass itemObject = new ItemClass();
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyUp(KeyCode.I))
+		{
+			showInventory = !showInventory;
+		}
+	}
+
+	void OnGUI()
+	{
+		if (showInventory) 
+		{
+			InventoryWindowRect = GUI.Window(0, InventoryWindowRect, InventoryWindowMethod, "");
+		}
+	}
+
+	void InventoryWindowMethod(int windowId)
+	{
+		for (int i = 0; i < inventorySize; i++)
+		{
+			if(inventoryList[i] != null)
+			{
+				if(GUI.Button( new Rect((Screen.height*0.1f)*i, 0, Screen.height*0.1f,Screen.height*0.1f), inventoryList[i].icon))
+				{
+					//Mit dem Object was machen
+					inventoryList[i] = null;
+				}
+			}
+			else
+			{
+				GUI.Button( new Rect((Screen.height*0.1f)*i, 0, Screen.height*0.1f,Screen.height*0.1f), string.Empty);
+			}
+		}
+	}
+
+}
