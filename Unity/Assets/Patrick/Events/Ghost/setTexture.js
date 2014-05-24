5e4cb38dcf4e925f2bb983a9ad98c5bbc06b0897
Fuby <patrick.steidinger@hotmail.de>
2014-05-24 20:40:06 +0200

    Ghost_Random_Event
    Funktioniert, aber noch nicht wirklich hübsch...

---
+++ Unity/Assets/Patrick/Events/Ghost/setTexture.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var mainTexture : Texture2D;
+var textures :  Texture2D[];
+var changeInterval : float = 0.33;
+
+function Update ()
+{
+	if (textures.length == 0)
+	{return;}
+	var index : int = Time.time / changeInterval;
+	index = index%textures.length;
+	renderer.material.mainTexture = textures[index];
+}
+   
