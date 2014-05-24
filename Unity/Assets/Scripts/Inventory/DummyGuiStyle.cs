76e9ff43689b41149e4b27c29f53747b8e1f01b4
Alexander Scheurer <mail@aspepex.net>
2014-05-24 20:14:09 +0200

    Dummy Gui bla

--- Unity/Assets/Scripts/Inventory/DummyGuiStyle.cs
+++ Unity/Assets/Scripts/Inventory/DummyGuiStyle.cs
@@ -9,20 +9,18 @@ public class DummyGuiStyle : MonoBehaviour
     public Texture GuiBackground;
     public Texture GuiItemBackgroundUp;
     public Texture GuiItemBackgroundDown;
+    public Texture GuiItemBackgroundFocus;
     public Texture DummyContent;
 
     private bool[] _buttonActive;
     private Texture[] _buttonBackground;
+    private Texture[] _inventoryContent;
 
 	// Use this for initialization
 	void Start () {
 	    _buttonActive = new bool[10];
 	    _buttonBackground = new Texture[10];
-
-	    foreach (var i in _buttonActive)
-	    {
-	        //i = false;
-	    }
+        _inventoryContent = new Texture[10];
 	}
 	
 	// Update is called once per frame
@@ -30,18 +28,28 @@ public class DummyGuiStyle : MonoBehaviour
     {
         GUI.skin = myCustomSkinThing;
 
-        GUI.DrawTexture(new Rect(Screen.width / 2 - 160, Screen.height - 34, 32, 32), GuiItemBackgroundUp);
-        GUI.Button(new Rect(Screen.width / 2 - 160, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 - 128, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 - 96, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 - 64, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 - 32, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 - 0, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 + 32, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 + 64, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 + 96, Screen.height - 34, 32, 32), "Bla");
-        GUI.Button(new Rect(Screen.width / 2 + 128, Screen.height - 34, 32, 32), "Bla");
+        _buttonActive[5] = true;
+        _inventoryContent[5] = DummyContent;
 
+        for (int i = 0; i < 10; i++)
+        {
+            if (_buttonActive[i])
+            {
+                _buttonBackground[i] = GuiItemBackgroundFocus;
+            }
+            else if (_inventoryContent[i] != null)
+            {
+                _buttonBackground[i] = GuiItemBackgroundDown;
+            }
+            else
+            {
+                _buttonBackground[i] = GuiItemBackgroundUp;
+            }
+
+
+            _buttonActive[i] = GUI.Button(new Rect(Screen.width / 2 - 160 + (i*32), Screen.height - 34, 32, 32), _inventoryContent[i]);
+            GUI.DrawTexture(new Rect(Screen.width / 2 - 160 + (i * 32), Screen.height - 34, 32, 32), _buttonBackground[i]);
+        }
         
         GUI.DrawTexture(new Rect(Screen.width/2 - 180,Screen.height - 36,360,36), GuiBackground);
 
