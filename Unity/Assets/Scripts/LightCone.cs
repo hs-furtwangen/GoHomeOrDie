3f01cdf1ad4933fb13635bf024f7f3b345bd6a00
Jan Möller <jan.moeller@hs-furtwangen.de>
2014-05-25 01:14:53 +0200

    Added a cone of light

---
+++ Unity/Assets/Scripts/LightCone.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightCone : MonoBehaviour {
+
+	public GameObject player;
+	public GameObject fogOfWar;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.position = player.transform.position;
+		var radius = fogOfWar.GetComponent<FogOfWar>().RevInnerRadius;
+		this.transform.localScale = new Vector3 (100.0f / radius, 100.0f / radius, 100.0f / radius);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.transform.position = player.transform.position;
+		var radius = fogOfWar.GetComponent<FogOfWar>().RevInnerRadius;
+		this.transform.localScale = new Vector3 (100.0f / radius, 100.0f / radius, 100.0f / radius);
+	}
+}
